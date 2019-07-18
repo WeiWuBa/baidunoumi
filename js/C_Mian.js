6ea5bc29c6652a187dc5925d99e1cb6cd95cda90
caomingdong <18270749739@163.com>
2019-07-18 22:01:33 +0800

    18号更新二

--- js/C_Mian.js
+++ js/C_Mian.js
@@ -1,5 +1,18 @@
 $ (function(){
-	
+$.ajax({
+	data:{
+		
+	},
+	dataType:'json',//服务器返回json格式数据
+	type:'post',//HTTP请求类型
+	timeout:10000,//超时时间设置为10秒；
+	success:function(data){
+		
+	},
+	error:function(xhr,type,errorThrown){
+		
+	}
+});
 	
 	
 	
