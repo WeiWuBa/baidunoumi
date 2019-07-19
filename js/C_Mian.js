a9f699f6b0a1a50d3d5b24922ff4e774856fd2ab
caomingdong <18270749739@163.com>
2019-07-19 21:19:25 +0800

    19日第一次修改

--- js/C_Mian.js
+++ js/C_Mian.js
@@ -1,20 +1,27 @@
 $ (function(){
-$.ajax({
-	data:{
-		
-	},
-	dataType:'json',//服务器返回json格式数据
-	type:'post',//HTTP请求类型
-	timeout:10000,//超时时间设置为10秒；
-	success:function(data){
-		
-	},
-	error:function(xhr,type,errorThrown){
-		
-	}
-});
 	
 	
+	$('#C_hotelSearchCity').blur(function(){
+		$.ajax({
+			url:'https://t.nuomi.com/sz',
+			// data:{
+			// 	
+			// },
+			dataType:'json',//服务器返回json格式数据
+			type:'get',//HTTP请求类型
+			success:function(data){
+				console.log(data);
+			},
+			error:function(xhr,type,errorThrown){
+				
+			}
+		});
+	});
+	$('C_hotelCityDetail_tab').on('click',this,function(){
+		alert(1);
+	});
+	
+
 	
 	
 	
