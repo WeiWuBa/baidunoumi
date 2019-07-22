abcc3b38334e0da10b17028964963e50df778e5e
caomingdong <18270749739@163.com>
2019-07-22 21:15:45 +0800

    基本功能完成

--- js/C_hotel_Mian.js
+++ js/C_hotel_Mian.js
@@ -1,7 +1,5 @@
 $(function(){
 
-
-
 /* ———————————————————————————————酒店页面吸顶效果———————————————————————————————————— */
 $(window).scroll(function(){
 	var C_contentTop_t = $(window).scrollTop();
@@ -17,11 +15,54 @@ $(window).scroll(function(){
 $('.C_contentTop_list').on('click','a',function(){
 	$('.C_contentTop_list a').removeClass('C_contentTop_list_hover');
 	$(this).toggleClass('C_contentTop_list_hover');
-})
+});
+
+/* ———————————————————————————————酒店地点选择———————————————————————————————————— */
+$('.C_hotelCityDetail_tab').on('click','a',function(){
+	//三角小标
+	$('.C_hotelCityDetail_tab a span').css('display','none');
+	$(this).find('span').css('display','inline-block');
+	//text值
+	function GetCity(as){
+		if(as == '地铁附近'){
+			return 'ditiefujin';
+		}else if(as == '行政区'){
+			return 'xingzhengqu';
+		}
+	}
+	var Get_City = GetCity($(this).text());
+	$.ajax({
+		url:'json/hotel/C_hotelCity.json',
+		dataType:'json',//服务器返回json格式数据
+		type:'get',//HTTP请求类型
+		cache:false,//是否使用缓存
+		success:function(data){
+			var shop ='';//清空shop
+			$.each(data,function(index,element){
+				shop +=	'<div class="C_hotelCityDetail_tab">';
+				for (var i = 0; i < element.length; i++) {
+					shop +=	'<a href="#">'+element[i]+'</a>';
+				}
+				shop +=	'</div>';
+			});
+			$('.C_hotelCityDetail_areaDetail').html(shop);
+		},
+		error:function(xhr,type,errorThrown){
+			alert("请求失败，请重试！！")
+		}
+	});
+});
+//默认全选状态
+
+
+
+
+
+
 /* ———————————————————————————————酒店页面店家展示———————————————————————————————————— */
 
 $.ajax({
-	url:'json/C_hotel.json',
+	url:'json/hotel/C_hotel.json',
 	dataType:'json',//服务器返回json格式数据
 	type:'get',//HTTP请求类型
 	cache:false,//是否使用缓存
@@ -30,9 +71,10 @@ $.ajax({
 		var conet = 0;
 		$.each(data,function(index,element){
 			conet++;
-			shop += '<li><!-- 酒店logo图 --><a href="#" class="clearfix"><img src="'+element.hotel_img_url+'" /></a><div class="C_contenthotel_listcontent"><!-- 酒店名称 --><a href="#"><h3>'+element.hotel_name+'</h3></a><!-- 酒店星形评价 --><p><a href="#"><img src="images/hotel/shop-star-b_767a724.png" /><span><img src="images/hotel/shop-star-o_e5d6259.png" /></span><span>4分</span></a><!-- 酒店价格 --><span>'+element.hotel_price+'</span></p><!-- 酒店文字评价 --><span><a href="#">'+element.hotel_appraise+'</a></span><!-- 酒店地址 --><p><i class="iconfont">&#xe60c;</i>'+element.hotel_site+'<span>'+element.hotel_address+'</span></p></div></li>';
+			shop += '<li class="C_Data" code="'+element.hotel_id+'"><!-- 酒店logo图 --><a href="hotel_subpage.html" class="clearfix"><img src="'+element.hotel_img_url+'" /></a><div class="C_contenthotel_listcontent"><!-- 酒店名称 --><a href="hotel_subpage.html"><h3>'+element.hotel_name+'</h3></a><!-- 酒店星形评价 --><p><a href="hotel_subpage.html"><img src="images/hotel/shop-star-b_767a724.png" /><span><img src="images/hotel/shop-star-o_e5d6259.png" /></span><span>4分</span></a><!-- 酒店价格 --><span>'+element.hotel_price+'</span></p><!-- 酒店文字评价 --><span><a href="#">'+element.hotel_appraise+'</a></span><!-- 酒店地址 --><p><i class="iconfont">&#xe60c;</i>'+element.hotel_site+'<span>'+element.hotel_address+'</span></p></div></li>';
 		});
 		$('.C_contenthotel_list').html(shop);
+		$('.C_contentBot_pagingT1').html(conet);
 	},
 	error:function(xhr,type,errorThrown){
 		alert("请求失败，请重试！！")
@@ -72,8 +114,24 @@ $.ajax({
 // });
 // 
 
+/* ———————————————————————————————数据存储到cookie————————————————————————————— */
+$('.C_contenthotel_list').on('click','li a',function (){
+	var code = $(this).parent().attr('code') || $(this).parent().parent().attr('code') || $(this).parent().parent().parent().attr('code');
+	if (localStorage.getItem('C_Data')) {
+		// 获取本地存储的数据[]
+		var codeArr = JSON.parse(localStorage.getItem('C_Data')).code;
+	} else {
+		var codeArr = [];
+	}
+	codeArr.push(code);
+	console.log(codeArr);
+	// 把数据更新到本地存储
+	var jsonStr = JSON.stringify({"code":codeArr});
+	console.log(jsonStr);
+	localStorage.setItem('C_Data',jsonStr);
+});
 
-
+	
 	
 	
 	
--- js/C_hotel_subpage.js
+++ js/C_hotel_subpage.js
@@ -1,14 +1,45 @@
 $(function(){
 	
 /* ———————————————————————————————二级页面获取一级页面数据————————————————————————————— */
-	// 1获取图片
-	// 2获取酒店名称
-	// 3获取人均价格
-	// 4获取地址
-	$('.C_mian_top_name').html();
 
-
-
+if (localStorage.getItem('C_Data')) {
+	// 获取本地数据[]
+	var codeArr = JSON.parse( localStorage.getItem('C_Data') ).code;
+	if (codeArr.length == 0) {//判断是否有数据
+		return false;
+	}
+	$.ajax({
+		url: 'json/hotel/C_hotel.json',
+		dataType:'json',//服务器返回json格式数据
+		type:'get',//HTTP请求类型
+		cache:false,//是否使用缓存
+		success: function (data){
+			$.each(codeArr,function (i,item){
+				$.each(data,function (index,element){
+					if (item == element.hotel_id) {
+						// 1获取图片
+						// 2获取酒店名称
+						// 3获取人均价格
+						// 4获取地址
+						// 5随机电话号码
+						// 6随机售出额
+						$('.C_mian_top_name').text(element.hotel_name);
+						$('.C_mian_top_site').text(element.hotel_address);
+						$('.C_mian_bot_shopping_sell1').text(getRand(50,500));
+						$('.C_mian_bot_shopping_sell2').text(getRand(50,200));
+						$('.C_mian_top_site').text(element.hotel_address);
+						$('.C_mian_top_img').html('<img src="'+element.hotel_img_url+'"/>');
+						$('.C_mian_bot_shopping_img1').html('<img src="'+element.hotel_img_url1+'"/>');
+						$('.C_mian_bot_shopping_img2').html('<img src="'+element.hotel_img_url2+'"/>');
+						$('.C_mian_top_phone').html(getRand(11111111111,999999999999));
+						
+					}
+				});
+			});
+		}
+	});
+}
+	
 /* ———————————————————————————————鼠标滑过地图显示标签————————————————————————————— */
 	$('.C_mian_bot_R_map').mouseover(function(){
 		$(this).find('a').css('opacity','1');
