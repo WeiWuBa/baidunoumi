4c082f1fccabf1919596deae66859179b6a42db2
caomingdong <18270749739@163.com>
2019-07-24 11:59:51 +0800

    24日第一次保存

--- js/C_hotel_Mian.js
+++ js/C_hotel_Mian.js
@@ -1,9 +1,12 @@
 
 $(function(){
-	$('.contentTop_checek').on('click',function(){
-		location.reload();
-	})
+
 /* ———————————————————————————————入住城市选择———————————————————————————————————— */
+// 热门城市选择
+$('.C_hotelCityLeft_inp').on('click','ul li',function(){
+	$('.C_hotelCityLeft_inp ul li').removeClass('C_hotelCityLeft_inpulli_hover');
+	$(this).toggleClass('C_hotelCityLeft_inpulli_hover');
+});
 $('#C_hotelSearchCity').click(function(e){
 	e.stopPropagation();
 	$('.C_hotelCityLeft_inp').css({'display':'block'});
@@ -11,19 +14,15 @@ $('#C_hotelSearchCity').click(function(e){
 $('body').click(function(e){
 	e.stopPropagation();
 	$('.C_hotelCityLeft_inp').css({'display':'none'});
+	$.cookie('num','123')
 });
-// $("body").click(function (e) {
-// 	if (!$(e.target).closest("#C_hotelSearchCity").length) {
-// 		$(".C_hotelCityLeft_inp").hide();
-// 	}
-// });
-// 热门城市选择
-$('.C_hotelCityLeft_inp').on('click','ul li',function(){
-	$('.C_hotelCityLeft_inp ul li').removeClass('C_hotelCityLeft_inpulli_hover');
-	$(this).toggleClass('C_hotelCityLeft_inpulli_hover');
+$('.C_hotelCityLeft_inp').on('click','table tr td a',function(){
+	$('#C_hotelSearchCity').val($(this).text());
+	console.log($(this).text());
 });
+
 	
-/* ———————————————————————————————酒店页面吸顶效果———————————————————————————————————— */
+/* ———————————————————————————————酒店页面吸顶效果———————————————————————————————— */
 $(window).scroll(function(){
 	var C_contentTop_t = $(window).scrollTop();
 	if( C_contentTop_t > 490){
@@ -39,35 +38,17 @@ $('.C_contentTop_list').on('click','a',function(){
 	$('.C_contentTop_list a').removeClass('C_contentTop_list_hover');
 	$(this).toggleClass('C_contentTop_list_hover');
 });
-<<<<<<< HEAD
 //保存选中状态
 $('.contentTop_checek').click(function(){
+	$.cookie('checekId',null);
 	var val = $(this).val();
-	var arr = $.cookie("checkval").split(",");
-	arr.forEach(function(item,index){
-		if(val == item){
-			arr.splice(index,1);
-		}
-	})
+	var arr = val.split(",");
+	$.cookie('checekId',val);
+	// location.reload();
+	if($.cookie('checekId') == $('.contentTop_checek').val()){
+		$(this).attr('checked');
+	}
 });
-=======
-// 复选框状态保存
-// $('.contentTop_checek').on('click',function(){
-// 	var val = $(this).val();
-// 	var array = $.cookie('checkValue').split(',');
-// 	array.forEach(function(item,index){
-// 		if(val == item){
-// 			array.splice(index,1);
-// 		}
-// 	});
-// 	if ($(this).prop('checked')) {
-// 		array.push($(this).val());
-// 	}
-// 	$.cookie('checkval',array);
-// 	console.log($.cookie('checkValue',array));
-// });
-
->>>>>>> CMD
 
 /* ———————————————————————————————酒店地点选择———————————————————————————————————— */
 $('.C_hotelCityDetail_tab').on('click','a',function(){
@@ -75,9 +56,9 @@ $('.C_hotelCityDetail_tab').on('click','a',function(){
 	$('.C_hotelCityDetail_tab a span').css('display','none');
 	$('.C_hotelCityDetail_areaDetail').css('display','block');
 	$(this).find('span').css('display','inline-block');
-	//text值
+	//获取text值
 	function GetCity(as){
-		if(as == '地铁附近'){
+		if(as == '地铁附近'){//返回所点击a的值
 			return 'ditiefujin';
 		}else if(as == '行政区'){
 			return 'xingzhengqu';
@@ -91,13 +72,15 @@ $('.C_hotelCityDetail_tab').on('click','a',function(){
 		cache:false,//是否使用缓存
 		success:function(data){
 			var shop ='';//清空shop
-			$.each(data,function(index,element){
-				shop +=	'<div class="C_hotelCityDetail_tab">';
-				for (var i = 0; i < element.length; i++) {
-					shop +=	'<a href="#">'+element[i]+'</a>';
-				}
-				shop +=	'</div>';
-			});
+				$.each(data,function (index,element) {//遍历data
+					if(index == Get_City){//ture
+						shop +=	'<div C_cityid="'+element.C_cityid+'" class="C_hotelCityDetail_tab">';
+						for (var i = 0; i < element.length; i++) {
+							shop +=	'<a href="#">'+element[i]+'</a>';
+						}
+						shop +=	'</div>';
+					}
+				});
 			$('.C_hotelCityDetail_areaDetail').html(shop);
 		},
 		error:function(xhr,type,errorThrown){
@@ -105,10 +88,10 @@ $('.C_hotelCityDetail_tab').on('click','a',function(){
 		}
 	});
 });
-
 /* ———————————————————————————————默认全选状态———————————————————————————————————— */
+// 分类
 $('.C_hotelCityDetail_tab2').find('span a').click(function(){
-	$('.C_hotelCityDetail_tab2').parent().find('a').removeClass('C_hotelCityDetail_fuoce_click');s
+	$('.C_hotelCityDetail_tab2').parent().find('a').removeClass('C_hotelCityDetail_fuoce_click');
 	$(this).parent().siblings().find('a').removeClass('C_hotelCityDetail_fuoce_click');
 	$(this).toggleClass('C_hotelCityDetail_fuoce_click');
 });
@@ -125,7 +108,7 @@ $('.C_hotelCityDetail_fuoce2').find('a').click(function(){
 	$(this).parent().siblings().find('a').removeClass('C_hotelCityDetail_fuoce_click');
 });
 
-/* ———————————————————————————————酒店页面店家展示———————————————————————————————————— */
+/* ———————————————————————————————酒店页面店家展示———————————————————————————————— */
 
 $.ajax({
 	url:'json/hotel/C_hotel.json',
@@ -147,7 +130,21 @@ $.ajax({
 		alert("请求失败，请重试！！")
 	}
 });
-
+/* ———————————————————————————————数据存储到cookie————————————————————————————— */
+$('.C_contenthotel_list').on('click','li a',function (){
+	var code = $(this).parent().attr('code') || $(this).parent().parent().attr('code') || $(this).parent().parent().parent().attr('code');
+	if (localStorage.getItem('C_Data')) {// 获取本地存储的数据[]
+		var codeArr = JSON.parse(localStorage.getItem('C_Data')).code;
+	} else {
+		var codeArr = [];
+	}
+	codeArr.push(code);
+	console.log(codeArr);
+	// 把数据更新到本地存储
+	var jsonStr = JSON.stringify({"code":codeArr});
+	console.log(jsonStr);
+	localStorage.setItem('C_Data',jsonStr);
+});
 /* ———————————————————————————————酒店分页器功能———————————————————————————————————— */
 // var pageNum = 1; 	// 页数
 // var sumCount = 0;	// 总条数
@@ -180,24 +177,6 @@ $.ajax({
 // 	}
 // });
 // 
-
-/* ———————————————————————————————数据存储到cookie————————————————————————————— */
-$('.C_contenthotel_list').on('click','li a',function (){
-	var code = $(this).parent().attr('code') || $(this).parent().parent().attr('code') || $(this).parent().parent().parent().attr('code');
-	if (localStorage.getItem('C_Data')) {
-		// 获取本地存储的数据[]
-		var codeArr = JSON.parse(localStorage.getItem('C_Data')).code;
-	} else {
-		var codeArr = [];
-	}
-	codeArr.push(code);
-	console.log(codeArr);
-	// 把数据更新到本地存储
-	var jsonStr = JSON.stringify({"code":codeArr});
-	console.log(jsonStr);
-	localStorage.setItem('C_Data',jsonStr);
-});
-
 /* ———————————————————————————————数据分页显示————————————————————————————— */
 	// 绑定点击页码事件
 	$('.C_contentBot_pagination').on('click','li a',function(){
